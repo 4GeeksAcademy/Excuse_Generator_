7a7e1de9bd9f17a91c53dbc882642efcacad1eaf
Aitana Rodriguez <144685094+AitanaRQ@users.noreply.github.com>
2024-02-29 01:24:17 +0000

    corregido

--- src/app.js
+++ src/app.js
@@ -4,8 +4,38 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
+
+// Se declaran las constantes
+// 1*
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+// 2*
+const action = ["ate", "peed", "crushed", "broke"];
+// 3*
+const what = ["my homework", "the keys", "the car", "the computer"];
+// 4*
+const when = [
+  "before the class",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function generateExcuse() {
+  const whoItem = getItem(who);
+  const actionItem = getItem(action);
+  const whatItem = getItem(what);
+  const whenItem = getItem(when);
+
+  return whoItem + " " + actionItem + " " + whatItem + " " + whenItem;
+}
